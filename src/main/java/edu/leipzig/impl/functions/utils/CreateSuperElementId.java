f5373ec88c49ca0220561d695ee24506b6dfb070
Maximilian Zimmer <mz57waru@studserv.uni-leipzig.de>
2023-01-12 13:35:26 +0100

    Add annotations for CreateSuperElementId

--- src/main/java/edu/leipzig/impl/functions/utils/CreateSuperElementId.java
+++ src/main/java/edu/leipzig/impl/functions/utils/CreateSuperElementId.java
@@ -1,7 +1,12 @@
 package edu.leipzig.impl.functions.utils;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.flink.table.annotation.DataTypeHint;
+import org.apache.flink.table.annotation.FunctionHint;
+import org.apache.flink.table.annotation.HintFlag;
+import org.apache.flink.table.annotation.InputGroup;
 import org.apache.flink.table.functions.ScalarFunction;
+import org.gradoop.common.model.impl.properties.PropertyValue;
 
 /**
  * The implementation of the creating the super element id as the hash value from the selecting grouping
@@ -15,7 +20,7 @@ public class CreateSuperElementId extends ScalarFunction {
      * @param fields the objects to consider for building the hash
      * @return new super element id
      */
-    public String eval(Object... fields) {
+    public String eval(@DataTypeHint(inputGroup = InputGroup.ANY) Object... fields) {
         StringBuilder groupingKey = new StringBuilder();
         for (Object field : fields) {
             if (null != field) {
