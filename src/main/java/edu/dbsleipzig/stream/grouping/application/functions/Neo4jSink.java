f22b067657aaf2acfc4845f311c8c45908b6f2f2
Kevin Gomez <kevin.gomez@neo4j.com>
2024-11-04 09:46:10 +0100

    added remaning properties

--- src/main/java/edu/dbsleipzig/stream/grouping/application/functions/Neo4jSink.java
+++ src/main/java/edu/dbsleipzig/stream/grouping/application/functions/Neo4jSink.java
@@ -51,17 +51,32 @@ public class Neo4jSink extends RichSinkFunction<StreamTriple> {
   }
 
   private void writeTriple(StreamTriple streamTriple) {
+
+    // Example Triple
+    // (a5fe0f3163f3a33ce729b2ca5adea71fbb163934(t:2024-11-03 10:59:59.999)
+    // :Station{id=4:91257e73-d4e5-4b16-9aa6-5f9f5f07f8f3:1188:String})
+    // -[c394c5b0edda779daec4c002978a911600267b4d(t:2024-11-03 10:59:59.999):
+    // TRIP{minDuration=24:Integer,count=1:Long,operation=CREATE:String,maxDuration=24:Integer,avgDuration=24.0:Double}]
+    // ->(19350b6e0ba60ca71b92a6929c00860883fbaf5f(t:2024-11-03 10:59:59.999)
+    // :Station{id=4:91257e73-d4e5-4b16-9aa6-5f9f5f07f8f3:373:String})
+
     String query = "MATCH (s1:Station), (s2:Station) WHERE elementId(s1) = $srcId AND elementId(s2) = $trgId " +
       "WITH s1, s2 " +
       "CREATE (s1)-[t:CDC_TRIP]->(s2) " +
       "SET t.operation = $operation, " +
-      "t.count = $count ";
+      "t.minDuration = $minDuration, " +
+      "t.maxDuration = $maxDuration, " +
+      "t.avgDuration = $avgDuration, " +
+      "t.count = $count";
 
     Map<String, Object> params = new HashMap<>();
     params.put("srcId", streamTriple.getSource().getVertexId());
     params.put("trgId", streamTriple.getTarget().getVertexId());
     params.put("count", streamTriple.getProperties().get("count"));
-
+    params.put("minDuration", streamTriple.getProperties().get("minDuration"));
+    params.put("maxDuration", streamTriple.getProperties().get("maxDuration"));
+    params.put("avgDuration", streamTriple.getProperties().get("avgDuration"));
+    params.put("operation", streamTriple.getProperties().get("operation"));
 
     this.session.run(query, params);
   }
